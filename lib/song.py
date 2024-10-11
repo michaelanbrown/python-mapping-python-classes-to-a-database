b6cde3083ab5ff5a3453d5b119578df81b5e6b9a
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-11 14:56:46 -0400

    added the create table class method

--- lib/song.py
+++ lib/song.py
@@ -5,3 +5,15 @@ class Song:
     def __init__(self, name, album):
         self.name = name
         self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
