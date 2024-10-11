49abe0cd39fe7f11348391f086d1b9543ca24e35
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-11 15:00:22 -0400

    adding the provided create method which saves the data

--- lib/song.py
+++ lib/song.py
@@ -28,3 +28,9 @@ class Song:
         CONN.commit()
 
         self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
